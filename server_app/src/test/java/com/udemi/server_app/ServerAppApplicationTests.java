0f77cb485d464e539f74a3dfbdb34f3c02e25e66
Fateskink <shaynenguyen9@gmail.com>
2023-09-26 00:04:40 +0700

    server_app

---
+++ server_app/src/test/java/com/udemi/server_app/ServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemi.server_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
